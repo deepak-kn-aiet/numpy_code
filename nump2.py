63643ccc32d48acfde44aa391b941a5aae6a0f9f
deepak-kn-aiet <deepak-kn-aiet@users.noreply.github.com>
2026-05-24 13:59:08 +0530

    Add numpy codes

---
+++ nump2.py
@@ -0,0 +1,8 @@
+import numpy as np
+list1 = [1, 2, 3, 4, 5]
+list2 = ["john elder",41,list1, True]
+np1 = np.array([0, 1 , 2, 3, 4, 5 ,6 ,7 ,8.9])
+print(np1)
+print(np1.shape)
+np2 = np.arange(10)
+print(np2)

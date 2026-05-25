f2ded04a9c5de4838b63318a6a08787415592a97
user.deepak <user.deepak295@gmail.com>
2026-05-25 18:12:06 +0530

    Add numpy4 and numpyy4 scripts

---
+++ numpyy4.py
@@ -0,0 +1,11 @@
+import numpy as np 
+a = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8,9])
+print(a[1::2])
+print(a[-3:-1])
+print(a[::3])
+print(a[1:5:2])
+b = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(b[1,2])
+print(b[0:1,1:3])
+print(b[0:2,1:3])
+

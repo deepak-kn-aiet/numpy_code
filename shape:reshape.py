ec1d8ff1f93a157140b7dfa48f9a650894d456cd
deepak-kn-aiet <deepak-kn-aiet@users.noreply.github.com>
2026-05-25 20:15:31 +0530

    Push current numpy code changes

---
+++ shape:reshape.py
@@ -0,0 +1,10 @@
+import numpy as np
+np1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+print(np1)
+print(np1.shape)
+np2 = np1.reshape(3,4)
+print(np2)
+print(np2.shape)
+np3 = np1.reshape(2,2,3)
+print(np3)
+print(np3.shape)    

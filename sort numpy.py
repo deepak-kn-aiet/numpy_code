c06ec63b2c03547d0118313b7d290b580a69f39b
user.deepak <user.deepak295@gmail.com>
2026-05-28 09:18:28 +0530

    Add sort numpy.py

---
+++ sort numpy.py
@@ -0,0 +1,16 @@
+import numpy as np 
+np1 = np.array([3,9,8,0,1,2])
+print(np1)
+print(np.sort(np1))
+
+np2 = np.array(["redduck","duck","red","blue","yellow"])
+print(np2)
+print(np.sort(np2))
+
+np3 = np.array([True,False,True,False])
+print(np3)
+print(np.sort(np3))
+
+np4 = np.array([[2,5,2,6,1,3],[1,4,7,8,9,0]])
+print(np4)
+print(np.sort(np4))
